b05b0926e2da27ad652698f2e7c2693a026e2bf3
Jacob Bieker <jacob@bieker.tech>
2018-10-04 11:48:42 +0200

    Add comments on what to plot next

--- catalog_builder.py
+++ catalog_builder.py
@@ -95,7 +95,7 @@ ascii.write(data, 'matched_sep18_p8.cat', format='fixed_width', delimiter=None, 
 
 # First plot the skymap of the data
 
-plt.scatter(full_catalog['ra'], full_catalog['dc'])
+plt.scatter(full_catalog['ra'], full_catalog['dc'], label='All galaxies')
 plt.ylabel("Dec")
 plt.xlabel("RA")
 
@@ -107,5 +107,20 @@ spec_redshift_cols = ["z_spec_3dh", "zm_vds", "zm_coeS", "zs_mor", "zm_ina", "zm
 print(full_catalog.colnames)
 spec_z_mask = full_catalog["z_spec_3dh"] > 0.001
 
-plt.scatter(full_catalog[spec_z_mask]['ra'], full_catalog[spec_z_mask]['dc'])
+plt.scatter(full_catalog[spec_z_mask]['ra'], full_catalog[spec_z_mask]['dc'], label='Spectrographic redshift')
+plt.legend()
 plt.show()
+
+
+# Now stellar mass distribution
+
+# Star Formation vs Stellar Mass
+
+# Star Formation vs Stellar Mass split by Z
+
+# Distribution of IRAC fluxes
+
+# Ra and Dec colorcoded by redshift distance
+
+# 1.1mm in RA and Dec
+
