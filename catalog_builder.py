db926346e5f5d62563d6e343b795d74d90b533a7
Jacob Bieker <jacob@bieker.tech>
2018-10-04 10:50:13 +0200

    Version one of catalog made

--- catalog_builder.py
+++ catalog_builder.py
@@ -2,6 +2,8 @@ import astropy.io.ascii as ascii
 import astropy.table
 from astropy.coordinates import SkyCoord, Angle, SkyOffsetFrame, ICRS
 from astropy import units as u
+from astropy.table import Table
+from astropy.io.ascii import FixedWidth
 import astropy.io.fits as fits
 import numpy as np
 import matplotlib.pyplot as plt
@@ -66,9 +68,23 @@ ags23 = SkyCoord(53.086623*u.deg, -27.810272*u.deg, frame='icrs')
 # TODO Convert to degress the ra and dec ones, then Skycoordinate
 
 franco_1mm = SkyCoord([ags1, ags2, ags3, ags4, ags5, ags6, ags7, ags8, ags9, ags10, ags11, ags12, ags13, ags14, ags15, ags16, ags17, ags18, ags19, ags20, ags21, ags22, ags23])
+One_One_mm = [1.90, 1.99, 1.84, 1.72, 1.56, 1.27, 1.15, 1.43, 1.25, 0.88, 1.34, 0.93, 0.78, 0.86, 0.80, 0.82, 0.93, 0.85, 0.69, 1.11, 0.64, 1.05, 0.98]
+One_one_err = [0.20, 0.22, 0.21, 0.20, 0.19, 0.18, 0.17, 0.22, 0.21, 0.15, 0.25, 0.18, 0.15, 0.17, 0.16, 0.17, 0.19, 0.18, 0.15, 0.24, 0.11, 0.22, 0.21]
 
+print(len(ra_dec))
+print(len(full_catalog))
 idx, d2d, d3d = franco_1mm.match_to_catalog_sky(ra_dec)
+missing_data = np.zeros((len(full_catalog)))
+print(len(missing_data))
+# put it all together at once?
+full_catalog['1.1mm'] = missing_data
+full_catalog['1.1mm_err'] = missing_data
+full_catalog['1.1mm'][idx] = One_One_mm
+full_catalog['1.1mm_err'][idx] = One_one_err
 
+only_one_one = full_catalog['raS', 'dcS', 'ra', 'dc', '1.1mm', '1.1mm_err']
+ascii.write(only_one_one, 'matched_sep18_p8.cat', format='fixed_width', delimiter=None, bookend=False, overwrite=True)
+exit()
 print(idx)
 print("----------------------------------------------------------------------------------------------------------------")
 print(d2d)
@@ -76,10 +92,18 @@ print(d2d)
 # Get the stuff now
 
 i = 0
+matched_ra = []
+missing_data = np.zeros((len(full_catalog['ra'])))
+missing_error = np.zeros(len(full_catalog['dc']))
+data = Table([full_catalog['raS'], full_catalog['dcS'], full_catalog['ra'], full_catalog['dc'], missing_data, missing_error], names=['raS', 'dcS', 'ra', 'dc', '1.1mm', '1.1mm_err'])
 for galaxy in idx:
+    data[galaxy]['1.1mm'] = One_One_mm[i]
+    data[galaxy]['1.1mm_err'] = One_one_err[i]
     print(full_catalog[galaxy])
     print(ra_dec[galaxy].ra.hms)
     print(d2d[i])
     i += 1
+
+ascii.write(data, 'matched_sep18_p8.cat', format='fixed_width', delimiter=None, bookend=False, overwrite=True)
 # TODO Make graphs of the redshift clustering, star mass, etc. using mean flux values as the cutoff
 
