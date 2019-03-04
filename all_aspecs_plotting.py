Fix delta_z calculation

all_aspecs_plotting.py
@@ -63,6 +63,22 @@ def get_observed_ghz(z, transition):
     observed_ghz = observed_nm.to(u.GHz, equivalencies=u.spectral())
     return observed_ghz
 
+def get_observed_ghz_true(z, transition):
+    """
+    Get the observed GHz for given redshift based on transition, from Wide ASPECS paper
+    :param z: Z to calculate for
+    :param transition:
+    :return:
+    """
+    emitted_ghz = transitions[transition][2] * u.GHz
+
+    observed_ghz = emitted_ghz / (z + 1)
+
+    return observed_ghz
+
+print(get_observed_ghz(0.0030, "1-0"))
+print(get_observed_ghz_true(0.0030, "1-0"))
+
 #TODO Check Channel Width: ALTRVAL = 3.021582003656E+06 Hz
 
 def get_kms(channels, observed_ghz):
@@ -72,9 +88,7 @@ def get_kms(channels, observed_ghz):
     observed_ghz = observed_ghz * u.GHz
     width = channels * channel_width
 
-    #print(width)
-
-    #print(width * 299792.458)
+    #299792.458 = speed of light
     fwhm = (width * 299792.458) / observed_ghz
 
     fwhm = fwhm * (u.km / u.s)
@@ -122,6 +136,21 @@ def convert_to_rest_frame_ghz(z, ghz):
     :return:
     """
 
+    observed_ghz = ghz * u.GHz
+
+    emitted = observed_ghz * (z + 1)
+
+    return emitted
+
+
+def convert_to_rest_frame_ghz_true(z, ghz):
+    """
+    Take a measured GHz value, and calculates the restframe GHz value based on the given z of the matched galaxy
+    :param z:
+    :param ghz:
+    :return:
+    """
+
     # First step is to convert to nm rom rest rame GHz
 
     nm = (ghz * u.GHz).to(u.nm, equivalencies=u.spectral())
@@ -139,6 +168,23 @@ def convert_to_rest_frame_ghz(z, ghz):
 
     return final_ghz
 
+print(convert_to_rest_frame_ghz(1.334, 104.5160))
+print(convert_to_rest_frame_ghz_true(1.334, 104.5160))
+
+
+def convert_to_rest_frame_ghz(z, ghz):
+    """
+    Take a measured GHz value, and calculates the restframe GHz value based on the given z of the matched galaxy
+    :param z:
+    :param ghz:
+    :return:
+    """
+
+    observed_ghz = ghz * u.GHz
+
+    emitted = observed_ghz * (z + 1)
+
+    return emitted
 
 def get_delta_z(z, rest_ghz, ghz=None):
     """
@@ -153,10 +199,11 @@ def get_delta_z(z, rest_ghz, ghz=None):
     for key, values in transitions.items():
         if values[0] <= z <= values[1]:
             sghz = values[2] * u.GHz # Gets the GHz of the CO line
-            sghz = sghz.to(u.nm, equivalencies=u.spectral()) # Now Lambda_Emitted
-            rest_ghz = rest_ghz.to(u.nm, equivalencies=u.spectral()) # Lambda_Observed in rest frame
-            set_z = np.round(((sghz - rest_ghz) / rest_ghz), 3) # (Lambda_Observed - Lambda_Emitted) / Lambda_Emitted
-            # (Z+1)Lambda_Emitted = Lambda_Observed
+            rest_ghz /= (z+1)
+            set_z = np.round((sghz - rest_ghz)/ rest_ghz, 3) # (Freq_emitted - Freq_obs)/ Freq_obs = z
+
+            set_z = z - set_z
+
             print("Z: {} Set Z: {}".format(z, set_z))
             set_zs.append((key, set_z))
     set_z = np.min([np.abs(i[1]) for i in set_zs])
@@ -166,6 +213,8 @@ def get_delta_z(z, rest_ghz, ghz=None):
         if np.isclose(np.abs(element[1]),set_z):
             return element[1], element[0]
 
+print(convert_to_rest_frame_ghz(1.334, 104.5160) / (1.334+1))
+print(get_delta_z(1.334, convert_to_rest_frame_ghz(1.334, 104.5160)))
 
 aspecs_lines = Table.read("data/line_search_P3_wa_crop.out", format="ascii", header_start=0, data_start=1)
 
@@ -223,7 +272,7 @@ print("Distances: ")
 num_in_close = 0
 num_out = 0
 snr_limit = 6.
-z_sep = 0.3
+z_sep = 0.01
 catalog_ids = []
 aspecs_redshifts = []
 aspecs_no_fit = []

independent/plot.py
@@ -6,32 +6,47 @@ This is focused on the plotting, both of cutouts, SN, and others
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_signal_to_noise():
     return NotImplementedError
+
 
 def plot_single_cutout():
     return NotImplementedError
 
+
 def plot_multiple_cutouts():
     return NotImplementedError
+
 
 def plot_mstar_vs_sfr(type='square'):
     return NotImplementedError
 
+
 def plot_leinerdt():
     return NotImplementedError
 
+
 def whitaker_main_sequence(z, mass_start, mass_end):
+    """
+    Returns the main sequence fit for Mstar between mass_start and mass_end for a given Z from
+    Whitaker et al. 2014
+
+    :param z:
+    :param mass_start:
+    :param mass_end:
+    :return:
+    """
     mass_range = np.linspace(mass_start, mass_end, 100)
 
-    if z > 2.0:
-        sfr = -19.99 + 3.44*mass_range + -0.13*mass_range**2
+    if z > 2.0: # Valid till 2.5
+        sfr = -19.99 + 3.44 * mass_range + -0.13 * mass_range ** 2
     elif 1.5 < z <= 2.0:
-        sfr = -24.04 + 4.17*mass_range + -0.16*mass_range**2
+        sfr = -24.04 + 4.17 * mass_range + -0.16 * mass_range ** 2
     elif 1.0 < z <= 1.5:
-        sfr = -26.03 + 4.62*mass_range + -0.19*mass_range**2
-    elif z <= 1.0:
-        sfr = -27.4 + 5.02*mass_range + -0.22*mass_range**2
+        sfr = -26.03 + 4.62 * mass_range + -0.19 * mass_range ** 2
+    elif z <= 1.0: #Valid to 0.5
+        sfr = -27.4 + 5.02 * mass_range + -0.22 * mass_range ** 2
 
     return mass_range, sfr
 
@@ -45,7 +60,7 @@ def schrieber_main_sequence(z, mass_start, mass_end):
     But not for SFR output, because it equals log10(SFR) which is what is given
     """
 
-    r = np.log10(1+z)
+    r = np.log10(1 + z)
 
     mass_range = np.linspace(mass_start, mass_end, 100)
 
@@ -54,12 +69,11 @@ def schrieber_main_sequence(z, mass_start, mass_end):
     print(mass_end)
     print(np.max(mass_range))
 
-
-    m_0 = 0.5 #+-0.07
-    a_0 = 1.5 #+- 0.15
-    a_1 = 0.3 #+- 0.08
-    m_1 = 0.36 # +- 0.3
-    a_2 = 2.5 # +- 0.6
+    m_0 = 0.5  # +-0.07
+    a_0 = 1.5  # +- 0.15
+    a_1 = 0.3  # +- 0.08
+    m_1 = 0.36  # +- 0.3
+    a_2 = 2.5  # +- 0.6
 
     m_s = mass_range - 9.0
     print(np.min(m_s))
@@ -67,16 +81,17 @@ def schrieber_main_sequence(z, mass_start, mass_end):
 
     sfr = []
     for m in m_s:
-        sfr.append((m - m_0 + a_0*r - a_1*(np.max([0, (m-m_1-a_2*r)])**2)))
+        sfr.append((m - m_0 + a_0 * r - a_1 * (np.max([0, (m - m_1 - a_2 * r)]) ** 2)))
 
     return mass_range, sfr
 
+
 def create_points_and_error_by_z(column_base_name, initial_catalog, low_z, high_z):
-    z_mask = (initial_catalog["z_1"] >= low_z) & (initial_catalog["z_1"] <= high_z)
-    centerpoints = initial_catalog[z_mask][str(column_base_name + "_50_1")]
-    lower_error = initial_catalog[z_mask][str(column_base_name + "_16_1")]
-    upper_error = initial_catalog[z_mask][str(column_base_name + "_84_1")]
-    z_values = initial_catalog[z_mask]["z_1"]
+    z_mask = (initial_catalog["z"] >= low_z) & (initial_catalog["z"] <= high_z)
+    centerpoints = initial_catalog[z_mask][str(column_base_name + "_50")]
+    lower_error = initial_catalog[z_mask][str(column_base_name + "_16")]
+    upper_error = initial_catalog[z_mask][str(column_base_name + "_84")]
+    z_values = initial_catalog[z_mask]["z"]
     centerpoints = np.nan_to_num(centerpoints)
     lower_error = np.nan_to_num(lower_error)
     upper_error = np.nan_to_num(upper_error)
@@ -88,10 +103,11 @@ def create_points_and_error_by_z(column_base_name, initial_catalog, low_z, high_
     error_bars = [lower_error, upper_error]
     return centerpoints, error_bars, z_values
 
+
 def create_points_and_error(column_base_name, initial_catalog):
-    centerpoints = initial_catalog[str(column_base_name + "_50_1")]
-    lower_error = initial_catalog[str(column_base_name + "_16_1")]
-    upper_error = initial_catalog[str(column_base_name + "_84_1")]
+    centerpoints = initial_catalog[str(column_base_name + "_50")]
+    lower_error = initial_catalog[str(column_base_name + "_16")]
+    upper_error = initial_catalog[str(column_base_name + "_84")]
     centerpoints = np.nan_to_num(centerpoints)
     zero_mask = centerpoints != 0.0
     centerpoints = centerpoints[zero_mask]